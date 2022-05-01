c24cf2a811f1d8c5acf527b05f192009e5cf1957
Vegeta <56867717+pozengineer@users.noreply.github.com>
2022-05-01 17:46:53 +1000

    realTime chat feature completed format navbar tabs

--- public/assets/js/realTimeChat.js
+++ public/assets/js/realTimeChat.js
@@ -4,14 +4,27 @@ const messageContainer= document.getElementById('message-container')
 const messageForm= document.getElementById('send-container');
 const messageInput= document.getElementById('message-input');
 
+const userTag= prompt('What is your userTag')
+appendMessage('You joined')
+socket.emit('new-user', userTag)
+
 socket.on('chat-message', data => {
     console.log(data)
-    appendMessage(data)
+    appendMessage(`${data.userTag}: ${data.message}`)
+})
+
+socket.on('user-connected', userTag => {
+    appendMessage(`${userTag} connected`)
+})
+
+socket.on('user-disconnected', userTag => {
+    appendMessage(`${userTag} disconnected`)
 })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message= messageInput.value
+    appendMessage(`You: ${message}`)
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
