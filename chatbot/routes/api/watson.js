a7831936106b1fe236b4c132e3fdb8be1ca1977f
s3496168 <s3496168@student.rmit.edu.au>
2022-05-03 07:40:56 +1000

    still missing the apikey

--- chatbot/routes/api/watson.js
+++ chatbot/routes/api/watson.js
@@ -2,10 +2,14 @@
 const express = require("express");
 const router = express.Router();
 const AssistantV2 = require("ibm-watson/assistant/v2");
-const {IamAuthenticator} = require("ibm-watson/auth");
+const { IamAuthenticator } = require("ibm-watson/auth");
+
 /*2. Create instance of assistant*/
 /*2.1: First authenticate*/
-const authenticator = new IamAuthenticator({apikey: process.env.WATSON_ASSISTANT_APIKEY,}); 
+const authenticator = new IamAuthenticator({
+    apikey: process.env.WATSON_ASSISTANT_APIKEY,
+}); 
+
 /*2.2: Connect to assistant*/
 const assistant = new AssistantV2({
     version: "2021-11-27",
@@ -15,22 +19,44 @@ const assistant = new AssistantV2({
 
 /*3. Route to handle session tokens*/
 /*GET api/watson/session*/ 
-router.get("/session", async (req,res) => {
+router.get("/session", async (req, res) => {
     /*If successful*/
-    try{
+    try {
     const session = await assistant.createSession({
         assistantId: process.env.WATSON_ASSISTANT_ID,
     });
     res.json(session["result"]);
     /*error message for failures*/
-}catch(err){
-    res.send("Sorry, an error occurrued while processing your request");
+} catch (err) {
+    res.send("Sorry, an error occurrued while processing your request. The error lies within the routing.");
     console.log(err);
-
 }
 });
 /*4. Handle messages*/
-/*incoming*/
+/*POST api/watson/session*/ 
+router.post("/message", async (req, res) =>{
+    /*build payload*/
+    payload = {
+        assistantId: process.env.WATSON_ASSISTANT_ID,
+        sessionId: req.headers.session_id,
+        input: {
+            message_type: "test",
+            text: req.body.input,
+    },
+};
+  
+
+/*If successful*/
+try{
+    const message = await assistant.message(payload);
+    res.json(message["result"]);
+
+/*if not successful*/
+}catch (err){
+    res.send("Sorry, an error occurrued while processing your request. The error lies within the message handling.");
+    console.log(err);
+}
+});
 
 /*5. Export routes*/ 
 module.exports = router; 
--- chatbot/server.js
+++ chatbot/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 const watsonRoutes = require("./routes/api/watson")
 /*Direct requests to /routes/api/watson to Watson routes*/
 app.use("/api/watson/", watsonRoutes);
+
 /*3. start server*/
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
